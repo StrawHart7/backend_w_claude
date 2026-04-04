98a49fb9e6e269bf526e7dcf9d9bb8ed1c2f94a3
StrawHart7 <wisdomagbodeka482@gmail.com>
2026-04-04 16:48:20 +0000

    Add GET /auth/me route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { register, login, getMe } = require('../controllers/authController')
+const verifyToken = require('../middlewares/authMiddleware')
 
 router.post('/register', register)
 router.post('/login', login)
