3fa244cf5584d7f42f5a1c66688b69a7c0cff651
StrawHart <test@gmail.com>
2026-04-26 17:36:50 +0000

    v1.0.3

--- routes/todosRoutes.js
+++ routes/todosRoutes.js
@@ -6,7 +6,7 @@ const verifyToken = require('../middlewares/authMiddleware')
 
 router.get('/', verifyToken, getTodos)
 router.post('/', verifyToken, createTodo)
-roter.put('/:id', verifyToken, updateTodo)
+router.put('/:id', verifyToken, updateTodo)
 router.delete('/:id', verifyToken, deleteTodo)
 
 module.exports = router
