6f59fa2fdab8ff5188a5e76c01e8f550b3030172
StrawHart <test@gmail.com>
2026-05-05 19:06:01 +0000

    v1.4.05

--- routes/pushRoutes.js
+++ routes/pushRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { subscribe } = require('../controllers/pushController');
-const authMiddleware = require('../middlewares/authMiddleware');
 const { subscribe, testPush } = require('../controllers/pushController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/subscribe', authMiddleware, subscribe);
 router.get('/test', authMiddleware, testPush);
